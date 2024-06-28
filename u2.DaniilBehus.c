04460af4adf076e60e4cf4b2c8a802fdfe22304c
DaniilBehus <daniil.behus@gmail.com>
2024-06-28 16:44:18 +0200

    nove

--- u2.DaniilBehus.c
+++ u2.DaniilBehus.c
@@ -13,78 +13,158 @@ typedef struct {
     char *matrix;
 } NURELM;
 
-// Makro pre prístup k prvkom matice
-#define BIND(r, i, j) ((r)->matrix[(i) * ((r)->n) + (j)])
+// Funkcia na vytvorenie relacie s danou velkostou
+NURELM *nurelm_create(unsigned int m, unsigned int n) {
+    NURELM *rel = (NURELM *)malloc(sizeof(NURELM));
+    if (rel == NULL) return NULL;
+    rel->m = m;
+    rel->n = n;
+    rel->properties = 0;
+    rel->matrix = (char *)calloc(m * n, sizeof(char));
+    if (rel->matrix == NULL) {
+        free(rel);
+        return NULL;
+    }
+    return rel;
+}
 
-// Funkcia pre nastavenie prvku v matici
-char nurelm_set(NURELM *to_redef, unsigned int x, unsigned int y) {
-    // Kontrola na NULL ukazovatel
-    if (to_redef == NULL) {
-        printf("Error: NULL pointer provided.\n");
-        return IMPOSSIBLE;
+// Funkcia na vytvorenie prazdneho vztahu zo Z0 do Z0
+NURELM *nurelm_create_Znot_to_Znot(void) {
+    return nurelm_create(0, 0);
+}
+
+// Funkcia na vytvorenie a inicializaciu matice vztahu zo suboru
+NURELM *nurelm_create_by_file(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) return NULL;
+
+    NURELM *rel = (NURELM *)malloc(sizeof(NURELM));
+    if (rel == NULL) {
+        fclose(file);
+        return NULL;
     }
-    
-    // Kontrola, ci su x a y v pripustnych medziach
-    if (x >= to_redef->m || y >= to_redef->n) {
-        printf("Error: Indices out of bounds.\n");
-        return IMPOSSIBLE;
+
+    fscanf(file, "N R 1\n");
+    fscanf(file, "%u %u\n", &(rel->m), &(rel->n));
+
+    rel->matrix = (char *)calloc(rel->m * rel->n, sizeof(char));
+    if (rel->matrix == NULL) {
+        free(rel);
+        fclose(file);
+        return NULL;
     }
-    
-    // Nastavenie prvku v matici
-    BIND(to_redef, x, y) = 1;
-    
+
+    unsigned int i, j;
+    for (i = 0; i < rel->m; ++i) {
+        for (j = 0; j < rel->n; ++j) {
+            int temp;
+            fscanf(file, "%d", &temp);
+            BIND(rel, i, j) = temp;
+        }
+    }
+
+    fclose(file);
+    return rel;
+}
+
+// Funkcia na ulozenie maticoveho zobrazenia vztahu do suboru
+char nurelm_save(NURELM *rel, char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) return IMPOSSIBLE;
+
+    fprintf(file, "N R 1\n");
+    fprintf(file, "%u %u\n", rel->m, rel->n);
+
+    unsigned int i, j;
+    for (i = 0; i < rel->m; ++i) {
+        for (j = 0; j < rel->n; ++j) {
+            fprintf(file, "%d ", BIND(rel, i, j));
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
     return SUCCESS;
 }
 
-// Priklad pouzitia funkcie
-int main() {
-    // Zadanie velkosti matice pouzivatelom
-    unsigned int m, n;
-    printf("Zadajte velkost matice (m n): ");
-    scanf("%u %u", &m, &n);
-
-    // Vytvorenie prikladu struktury NURELM
-    NURELM example;
-    example.m = m;
-    example.n = n;
-    example.properties = 0;
-    example.matrix = (char *)calloc(example.m * example.n, sizeof(char));
-    if (example.matrix == NULL) {
-        printf("Error: Memory allocation failed.\n");
-        return 1;
+// Funkcia na spravne znicenie maticoveho zobrazenia vztahu v pamati
+void nurelm_destroy(NURELM *to_destroy) {
+    if (to_destroy != NULL) {
+        free(to_destroy->matrix);
+        free(to_destroy);
     }
-    
-    while (1) {
-        unsigned int x, y;
-        printf("Zadajte suradnice (x, y) pre nastavenie prvku v matici (velkost %ux%u) alebo zaporne cisla pre ukoncenie:\n", example.m, example.n);
-        scanf("%u %u", &x, &y);
-        
-        // Kontrola na ukoncenie
-        if (x == -1 || y == -1) {
-            break;
-        }
+}
 
-        // Nastavenie prvku v matici podla zadanych suradnic
-        char result = nurelm_set(&example, x, y);
-        if (result == SUCCESS) {
-            printf("Prvok bol úspešne nastavený.\n");
-        } else {
-            printf("Nepodarilo sa nastavit prvok.\n");
-        }
-    }
-    
-    // Vytlacenie matice pre kontrolu
-    unsigned int i, j;  // Deklarujeme premenne cyklu mimo cyklu
-    for (i = 0; i < example.m; ++i) {
-        for (j = 0; j < example.n; ++j) {
-            printf("%d ", BIND(&example, i, j));
+// Funkcia na vypis maticoveho zobrazenia vztahu na obrazovku
+void nurelm_print(NURELM *to_print) {
+    unsigned int i, j;
+    for (i = 0; i < to_print->m; ++i) {
+        for (j = 0; j < to_print->n; ++j) {
+            printf("%d ", BIND(to_print, i, j));
         }
         printf("\n");
     }
-    
-    // Uvolnenie alokovanej pamate
-    free(example.matrix);
-    
+}
+
+// Funkcia na zmenu zobrazenia vztahu *to_redef
+char nurelm_set(NURELM *to_redef, unsigned int x, unsigned int y) {
+    if (to_redef == NULL) {
+        return IMPOSSIBLE;
+    }
+    if (x >= to_redef->m || y >= to_redef->n) {
+        return IMPOSSIBLE;
+    }
+
+    BIND(to_redef, x, y) = 1;
+    return SUCCESS;
+}
+
+int main() {
+    unsigned int m, n, x, y;
+    printf("Zadajte pocet riadkov matice: ");
+    scanf("%u", &m);
+    printf("Zadajte pocet stlpcov matice: ");
+    scanf("%u", &n);
+
+    NURELM *rel = nurelm_create(m, n);
+    if (rel == NULL) {
+        printf("Chyba: Nedostatok pamate\n");
+        return 1;
+    }
+
+    int choice;
+    do {
+        printf("1. Nastavit hodnotu v matici\n");
+        printf("2. Vytlacit maticu\n");
+        printf("3. Ukoncit\n");
+        printf("Vyberte moznost: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Zadajte riadok: ");
+                scanf("%u", &x);
+                printf("Zadajte stlpec: ");
+                scanf("%u", &y);
+                if (nurelm_set(rel, x, y) == SUCCESS) {
+                    printf("Hodnota bola uspesne nastavena\n");
+                } else {
+                    printf("Nepodarilo sa nastavit hodnotu\n");
+                }
+                break;
+            case 2:
+                nurelm_print(rel);
+                break;
+            case 3:
+                printf("Ukoncenie programu\n");
+                break;
+            default:
+                printf("Neplatna volba\n");
+                break;
+        }
+    } while (choice != 3);
+
+    nurelm_destroy(rel);
     return 0;
 }
 
